2e4fb08e034c265182c432aadf155f56ad43db36
vicentekampff13 <vicentekampff@gmail.com>
2026-03-08 23:09:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 // ====== CONFIG ======
 const API_TOKEN = process.env.API_TOKEN || "12345";
